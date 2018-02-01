d221b61714126ad695beb4686bd4846d40915c3b
zzorin <ilya-zorin@yandex.ru>
2018-02-01 17:45:05 +0700

    Add task_path

--- Documents/python-work/task_path.py
+++ Documents/python-work/task_path.py
@@ -17,8 +17,8 @@ class Component:
 		return result
 
 	def iter_paths(self, class_path, specification):
-		# print(class_path)
-		class_obj = class_path[class_path.rfind("/")+1:]
+		# return list of paths from specification belongs to class path
+		class_obj = class_path[class_path.rfind("/")+1:]#name of class from path
 		specvalueslist = []
 		if class_obj in specification:
 			for specvalue in specification[class_obj]:
@@ -26,54 +26,69 @@ class Component:
 				specvalueslist.append(new_path_string)
 		return specvalueslist
 
+	def get_potential(self, class_path, specification):
+		result = []
+		queue = ["/" + class_path]
+		while queue:
+			result.extend(queue)
+			queue = self.get_chain_list(queue, specification)
+		return result
+
+	def get_chain_list(self, queue, specification):
+		queue = list(chain.from_iterable(
+			self.iter_paths(item, specification)
+			for item in queue
+		))
+		return queue
+
+
+
+	def get_applicable(self, class_path, specification):
+		queue_alg = [["/" + class_path]]
+		result = {}
+		while queue_alg:
+			for queue_alg_inner in queue_alg:
+				while queue_alg_inner:
+					queue_sum = []
+					ident_path = ''
+					temp_list = [] #list for paths with common part
+					queue_alg_inner = self.get_chain_list(queue_alg_inner, specification)
+					for item in queue_alg_inner:
+						queue_sum.append(item.split())
+						ident_path_current = item[:queue_alg_inner[0].rfind("/")]#get parent path
+						if ident_path_current==ident_path:		
+							# items have a common parent path
+							temp_list.append(item)
+							result[ident_path_current] = temp_list
+						else:
+							# items have different parent paths
+							ident_path = ident_path_current
+							temp_list.append(item)
+							result[ident_path_current] = item.split()
+				queue_alg = queue_sum
+		return result
+
 	def my_method(self, source_object):
 		result = {}
 		alg_dict = {}
 		potencial_list = []
+		class_name = source_object.__name__
 		result['Algorithm'] = alg_dict
 		for algorithm in self.algorithm_list:
 			alg_step_dict= {}
 			spec = {}
+			# get specification
 			for key,value in getattr(algorithm, 'SPECIFICATION').items():
 				values_list = []
 				for v in value:
 					values_list.append(v.__name__)
 					spec[key.__name__] = values_list
-			if source_object.__name__ in spec:
-				# Potential paths
-				queue = ["/" + source_object.__name__]
-				while queue:
-					potencial_list.extend(queue)
-					queue = list(chain.from_iterable(
-						self.iter_paths(item, spec)
-						for item in queue
-					))
-				# Algorithm paths
-				queue_alg = [["/" + source_object.__name__]]
-				
-				while queue_alg:
-					for queue_alg_inner in queue_alg:
-						while queue_alg_inner:
-							queue_sum = []
-							queue_alg_inner = list(chain.from_iterable(
-								self.iter_paths(inner_item, spec)
-								for inner_item in queue_alg_inner
-							))
-							for item in queue_alg_inner:
-								queue_sum.append(item.split())
-							ident_path = ''
-							temp_list = []
-							for item in queue_alg_inner:
-								ident_path_current = item[:queue_alg_inner[0].rfind("/")]
-								if ident_path_current==ident_path:		
-									temp_list.append(item)
-									alg_step_dict[ident_path_current] = temp_list
-								else:
-									ident_path = ident_path_current
-									temp_list.append(item)
-									alg_step_dict[ident_path_current] = item.split()
-						queue_alg = queue_sum
 
+			if class_name in spec:
+				# Potencial paths
+				potencial_list.extend(self.get_potential(class_name, spec))
+				# Algorithm paths
+				alg_step_dict = self.get_applicable(class_name, spec)
 
 			alg_dict[algorithm.__class__.__name__] = alg_step_dict
 		result['Potential'] = potencial_list
@@ -99,7 +114,7 @@ class Lemon3:
 class FirstAlgorithm:
 	SPECIFICATION = {
 	Orange: [Apple],
-	Lemon: [Orange, Apple]
+	Lemon: [Orange, Apple],
 	}
 	def __call__(self, source_object):
 		if isinstance(source_object, Orange):
