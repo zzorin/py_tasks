e8c182dbd195b5ec1f303f3b98ceb13ca532fe15
zzorin <ilya-zorin@yandex.ru>
2018-02-01 15:41:59 +0700

    Using itertools for paths

--- Documents/python-work/task_path.py
+++ Documents/python-work/task_path.py
@@ -7,37 +7,23 @@ class Component:
 	def __call__(self, source_object):
 		result = []
 		queue = [source_object]
-		# print(queue)
 		while queue:
-			# print(result)
 			result.extend(queue)
-			# print(queue)
 			queue = list(chain.from_iterable(
 				algorithm(item)
 				for item in queue
 				for algorithm in self.algorithm_list
 			))
-			print("queue")
-			print(queue)
 		return result
 
-	def iter_alg(self, alg_step_dict, path_string, class_obj, specification):
-		if class_obj in specification:
-			specvalueslist = []
-			for specvalue in specification[class_obj]:
-				new_path_string = path_string + '/' + specvalue
-				specvalueslist.append(new_path_string)
-				self.iter_alg(alg_step_dict, new_path_string, specvalue, specification)
-			alg_step_dict[path_string] = specvalueslist	
-		return alg_step_dict
-
-	def iter_potencial(self, path_string, class_obj, specification):
+	def iter_paths(self, class_path, specification):
+		# print(class_path)
+		class_obj = class_path[class_path.rfind("/")+1:]
 		specvalueslist = []
 		if class_obj in specification:
 			for specvalue in specification[class_obj]:
-				new_path_string = path_string + '/' + specvalue
+				new_path_string = class_path + "/" + specvalue
 				specvalueslist.append(new_path_string)
-				specvalueslist.extend(self.iter_potencial(new_path_string, specvalue, specification))
 		return specvalueslist
 
 	def my_method(self, source_object):
@@ -46,19 +32,50 @@ class Component:
 		potencial_list = []
 		result['Algorithm'] = alg_dict
 		for algorithm in self.algorithm_list:
-			alg_step_dict = {}
-			alg_dict[algorithm.__class__.__name__] = alg_step_dict
-			spec2 = {}
-			spec = getattr(algorithm, 'SPECIFICATION')
-			for key,value in spec.items():
+			alg_step_dict= {}
+			spec = {}
+			for key,value in getattr(algorithm, 'SPECIFICATION').items():
 				values_list = []
 				for v in value:
 					values_list.append(v.__name__)
-					spec2[key.__name__] = values_list
-			if source_object.__name__ in spec2:
-				potencial_list.append(source_object.__name__)
-				potencial_list.extend(self.iter_potencial("/" + source_object.__name__, str(source_object.__name__), spec2))
-			alg_dict[algorithm.__class__.__name__] = self.iter_alg({}, "/" + source_object.__name__, str(source_object.__name__), spec2)
+					spec[key.__name__] = values_list
+			if source_object.__name__ in spec:
+				# Potential paths
+				queue = ["/" + source_object.__name__]
+				while queue:
+					potencial_list.extend(queue)
+					queue = list(chain.from_iterable(
+						self.iter_paths(item, spec)
+						for item in queue
+					))
+				# Algorithm paths
+				queue_alg = [["/" + source_object.__name__]]
+				
+				while queue_alg:
+					for queue_alg_inner in queue_alg:
+						while queue_alg_inner:
+							queue_sum = []
+							queue_alg_inner = list(chain.from_iterable(
+								self.iter_paths(inner_item, spec)
+								for inner_item in queue_alg_inner
+							))
+							for item in queue_alg_inner:
+								queue_sum.append(item.split())
+							ident_path = ''
+							temp_list = []
+							for item in queue_alg_inner:
+								ident_path_current = item[:queue_alg_inner[0].rfind("/")]
+								if ident_path_current==ident_path:		
+									temp_list.append(item)
+									alg_step_dict[ident_path_current] = temp_list
+								else:
+									ident_path = ident_path_current
+									temp_list.append(item)
+									alg_step_dict[ident_path_current] = item.split()
+						queue_alg = queue_sum
+
+
+			alg_dict[algorithm.__class__.__name__] = alg_step_dict
 		result['Potential'] = potencial_list
 		return result
 
@@ -71,6 +88,12 @@ class Orange:
 		self.number = number
 
 class Lemon:
+	pass
+
+class Lemon2:
+	pass
+
+class Lemon3:
 	pass
 
 class FirstAlgorithm:
@@ -94,9 +117,8 @@ class EmptyAlgorithm:
 		return []
 
 component = Component(FirstAlgorithm(), EmptyAlgorithm())
-print(component(Lemon()))
 
-# print(json.dumps(component.my_method(Lemon), indent=4))
+print(json.dumps(component.my_method(Lemon), indent=4))
 # print(json.dumps( {'Potential': [
 #  	'/Lemon',
 # 	 '/Lemon/Orange',
